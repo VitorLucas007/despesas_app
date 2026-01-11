5c06157531cf30e9717981d91fa9231d812bc07d
vitorlucas007 <lucasyj64@gmail.com>
2026-01-11 03:11:21 -0300

    adiciona o campo icon na classe Categoria para suportar ícones personalizados

--- despesas_app/lib/models/categoria.dart
+++ despesas_app/lib/models/categoria.dart
@@ -1,9 +1,16 @@
 import 'package:despesas_app/models/despesa.dart';
+import 'package:flutter/material.dart';
 
 class Categoria {
   final String id;
   final String nome;
+  final IconData icon;
   final TipoTransacao tipo;
 
-  Categoria({required this.id, required this.nome, required this.tipo});
+  Categoria({
+    required this.id,
+    required this.nome,
+    required this.icon,
+    required this.tipo,
+  });
 }
