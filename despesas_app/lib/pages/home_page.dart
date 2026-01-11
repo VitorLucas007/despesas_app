a83b99519ce6af7932ad828368cafd186109db78
vitorlucas007 <lucasyj64@gmail.com>
2026-01-11 03:21:19 -0300

    refatora HomePage para usar StatelessWidget e recebe lista de despesas como parâmetro, simplificando o gerenciamento de estado

--- despesas_app/lib/pages/home_page.dart
+++ despesas_app/lib/pages/home_page.dart
@@ -1,53 +1,32 @@
 import 'package:despesas_app/models/despesa.dart';
 import 'package:despesas_app/models/resumo_financeiro.dart';
-import 'package:despesas_app/pages/add_despesa_page.dart';
 import 'package:despesas_app/widgets/despesas_tile_widget.dart';
 import 'package:despesas_app/widgets/resumo_card_widget.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class HomePage extends StatelessWidget {
+  final List<Despesa> despesas;
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  final List<Despesa> _despesas = [];
-
-  ResumoFinanceiro get _resumo => ResumoFinanceiro.fromDespesas(_despesas);
+  const HomePage({super.key, required this.despesas});
 
   @override
   Widget build(BuildContext context) {
+    final resumo = ResumoFinanceiro.fromDespesas(despesas);
+
     return Scaffold(
-      appBar: AppBar(title: Text('Minhas Despesas'), centerTitle: true),
+      appBar: AppBar(title: const Text('Minhas Despesas')),
       body: Column(
         children: [
-          ResumoCardWidget(entrada: _resumo.entrada, saida: _resumo.saida),
+          ResumoCardWidget(entrada: resumo.entrada, saida: resumo.saida),
           Expanded(
             child: ListView.builder(
-              itemCount: _despesas.length,
+              itemCount: despesas.length,
               itemBuilder: (context, index) {
-                return DespesasTileWidget(despesa: _despesas[index]);
+                return DespesasTileWidget(despesa: despesas[index]);
               },
             ),
           ),
         ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final despesa = await Navigator.push<Despesa>(
-            context,
-            MaterialPageRoute(builder: (context) => AddDespesaPage()),
-          );
-
-          if (despesa != null) {
-            setState(() {
-              _despesas.add(despesa);
-            });
-          }
-        },
-        child: Icon(Icons.add),
       ),
     );
   }
