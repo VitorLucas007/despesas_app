c921c8c7a26dde9ce9ef20744b4939ef2d5e98a3
vitorlucas007 <lucasyj64@gmail.com>
2026-01-19 18:13:21 -0300

    Adiciona inicialização assíncrona no método main para configurar o banco de dados e carregar categorias fixas ao iniciar o aplicativo.

--- despesas_app/lib/main.dart
+++ despesas_app/lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:despesas_app/services/init_service.dart';
 import 'package:despesas_app/widgets/main_navigation_page_widget.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Inicializa o banco de dados e carrega categorias fixas
+  final initService = InitService();
+  await initService.inicializar();
+
   runApp(const MyApp());
 }
 
